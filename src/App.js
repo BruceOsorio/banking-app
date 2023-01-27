22eee4db705c865534d969d352259d90ae45b665
Bruce Osorio <bruce@hostari.com>
2023-01-27 20:05:16 +0800

    new commit form for bank app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+function App() { // root component (inside body tag)
+  const [inputValue, setInputValue] = useState('')
+  const [accountABalance, setAccountABalance] = useState(0)
+  const [accountBBalance, setAccountBBalance] = useState(0)
+
+  // Event Handlers
+  const onDeposit = () => {
+    setAccountABalance(accountABalance + +inputValue)
+  }
+  const onWithdraw = () => {
+    setAccountABalance(accountABalance - +inputValue)
+  }
+  const onSendMoney = () => {
+    setAccountBBalance(accountBBalance + +inputValue)
+    setAccountABalance(accountABalance - +inputValue)
+  }
+
+  const onInputChange = (e) => {
+    setInputValue(e.target.value)
+  }
+
+
+  // JSX Return
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload ok?.
-        </p>
-
-        <p>
-          Hey look at me!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Account A: {accountABalance}</div>
+      <div>Account B: {accountBBalance}</div>
+      <input type="text" value={inputValue} onChange={onInputChange}></input>
+      <button onClick={onDeposit}>Deposit</button>
+      <button onClick={onWithdraw}>Withdraw</button>
+      <button onClick={onSendMoney}>Send Money</button>
     </div>
   );
 }
